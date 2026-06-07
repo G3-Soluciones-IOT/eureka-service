c432902b4e15cea890dfb7d2a86ffd88d8d65cf7
Jhon Galvez <jhon.galvezchambi@gmail.com>
2026-06-07 06:10:55 -0500

    feat(eureka-service): add eureka-service as standalone repository

---
+++ src/main/java/pe/edu/upc/eureka_service/EurekaServiceApplication.java
@@ -0,0 +1,16 @@
+package pe.edu.upc.eureka_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.actuate.autoconfigure.audit.AuditEventsEndpointAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication(exclude = {AuditEventsEndpointAutoConfiguration.class})
+@EnableEurekaServer
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
